a29fe1f6c42d7983ea220d56f724cc9b72d58d20
bhaveshgoyal27 <27goyalbhavesh@gmail.com>
2021-09-01 13:11:37 +0530

    final code

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -3,7 +3,38 @@ from django.contrib.auth.models import User
 import PIL
 from .utils import get_random_code
 from django.template.defaultfilters import slugify
+from django.shortcuts import reverse
+from django.db.models import Q
 # Create your models here.
+
+class ProfileManager(models.Manager):
+
+    def get_all_profiles_to_invite(self, sender):
+        profiles = Profile.objects.all().exclude(user=sender)
+        profile = Profile.objects.get(user=sender)
+        qs = Relationship.objects.filter(Q(sender=profile) | Q(receiver=profile))
+        print(qs)
+        print("#########")
+
+        accepted = set([])
+        for rel in qs:
+            if rel.status == 'accepted':
+                accepted.add(rel.receiver)
+                accepted.add(rel.sender)
+        print(accepted)
+        print("#########")
+
+        available = [profile for profile in profiles if profile not in accepted]
+        print(available)
+        print("#########")
+        return available
+        
+
+    def get_all_profiles(self, me):
+        profiles = Profile.objects.all().exclude(user=me)
+        return profiles
+
+
 class Profile(models.Model):
 	first_name = models.CharField(max_length=200, blank=True)
 	last_name = models.CharField(max_length=200, blank=True)
@@ -17,21 +48,25 @@ class Profile(models.Model):
 	updated = models.DateTimeField(auto_now=True) 
 	created =  models.DateTimeField(auto_now=True)
 
+	objects = ProfileManager()
+
 	def __str__(self):
 		return f"{self.user.username}-{self.created}"
 
 	def save(self,*args, **kwargs):
 		ex = False
-		if self.first_name and self.last_name:
-			to_slug = slugify(str(self.first_name)+ " " + str(self.last_name))
-			ex = Profile.objects.filter(slug=to_slug).exists()
-			while ex:
-				to_slug = slugify(to_slug+ " "+ str(get_random_code()))
+		to_slug = self.slug
+		if self.first_name != self.__initial_first_name or self.last_name != self.__initial_last_name or self.slug=="":
+			if self.first_name and self.last_name:
+				to_slug = slugify(str(self.first_name) + " " + str(self.last_name))
 				ex = Profile.objects.filter(slug=to_slug).exists()
-		else:
-			to_slug = str(self.user)
+				while ex:
+					to_slug = slugify(to_slug + " " + str(get_random_code()))
+					ex = Profile.objects.filter(slug=to_slug).exists()
+			else:
+				to_slug = str(self.user)
 		self.slug = to_slug
-		super().save(*args,**kwargs)
+		super().save(*args, **kwargs)
 
 	def get_friends(self):
 		return self.friends.all()
@@ -39,10 +74,48 @@ class Profile(models.Model):
 	def get_friends_no(self):
 		return self.friends.all().count()
 
+	def get_posts_no(self):
+		return self.posts.all().count()
+
+	def get_all_authors_posts(self):
+		return self.posts.all()
+
+	def get_likes_given_no(self):
+		likes = self.like_set.all()
+		total_liked = 0
+		for item in likes:
+			if item.value=="Like":
+				total_liked+=1
+		return total_liked
+
+	def get_likes_recieved_no(self):
+		posts = self.posts.all()
+		total_liked =0
+		for item in posts:
+			total_liked += item.liked.all().count()
+		return total_liked
+
+	def get_absolute_url(self):
+		return reverse("profiles:profile-detail-view", kwargs={"slug": self.slug})
+
+	__initial_first_name = None
+	__initial_last_name = None
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.__initial_first_name = self.first_name
+		self.__initial_last_name = self.last_name
+
+
 STATUS_CHOICES = (
     ('send', 'send'),
     ('accepted', 'accepted')
 )
+
+class RelationshipManager(models.Manager):
+    def invatations_received(self, receiver):
+        qs = Relationship.objects.filter(receiver=receiver, status='send')
+        return qs
 
 class Relationship(models.Model):
 	sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
@@ -50,6 +123,8 @@ class Relationship(models.Model):
 	status = models.CharField(max_length=8, choices=STATUS_CHOICES)
 	updated = models.DateTimeField(auto_now=True)
 	created = models.DateTimeField(auto_now_add=True)
+
+	objects = RelationshipManager()
 
 	def __str__(self):
 		return f"{self.sender}-{self.receiver}-{self.status}"
--- src/social_network/urls.py
+++ src/social_network/urls.py
@@ -23,7 +23,8 @@ from .views import home_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home_view, name='home_view'),
-    path('profiles/', include('profiles.urls',namespace='profiles'))
+    path('profiles/', include('profiles.urls',namespace='profiles')),
+    path('posts/', include('posts.urls', namespace='posts')),
 ]
 
 urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
